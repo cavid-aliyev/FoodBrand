63911cd0d226d7c1afc29baa9ed4e84bfc523e87
Alyeev <markmarkul123@gmail.com>
2021-01-26 15:26:27 +0400

    Create Modal and its Scroll properties

--- dist/js/script.js
+++ dist/js/script.js
@@ -98,6 +98,60 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('.timer', deadLine);
 
+    // Modal
+
+    const modalTrigger = document.querySelector('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modelTimerId);
+    }
+
+
+    modalTrigger.forEach(btn=>{
+        btn.addEventListener('click', openModal);
+    });
+
+
+
+    function closeModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = '';
+    }
+
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+
+    modal.addEventListener('click', (e)=>{
+        if(e.target === modal){
+           closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e)=>{
+        if(e.code === "Escape" && modal.classList.contains('show')){
+            closeModal();
+        }
+    });
+
+    const modelTimerId = setTimeout(openModal, 3000);
+
+
+    function showModalBySCroll(){
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            openModal();
+            window.removeEventListener('scroll', showModalBySCroll);
+        }
+    }
+
+    // if user scroll to the end show modal
+    window.addEventListener('scroll', showModalBySCroll);
 });
 
 
