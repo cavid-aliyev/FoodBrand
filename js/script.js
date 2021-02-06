b0bcd0931bb76747f5901c720baacb90fdedfc10
Alyeev <markmarkul123@gmail.com>
2021-02-06 10:47:20 +0400

    Using json-server

--- js/script.js
+++ js/script.js
@@ -303,7 +303,9 @@ window.addEventListener('DOMContentLoaded', function() {
         }, 4000);
     }
 
-
+    fetch('http://localhost:3000/menu  ')
+         .then(data => data.json())
+         .then(res => console.log(res));
 });
 
 
