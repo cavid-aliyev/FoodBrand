a04116bcb1864d8513875fb43108bd7e90ba6466
Alyeev <markmarkul123@gmail.com>
2021-02-12 12:17:53 +0400

    Change width slice to regexp function

--- js/script.js
+++ js/script.js
@@ -423,12 +423,17 @@ window.addEventListener('DOMContentLoaded', function () {
             indicators.append(dot);
             dots.push(dot);
         }
+
+
+        function deleteNotNumber(str){
+            return +str.replace(/\D/g, '');
+        }
     
         next.addEventListener('click', () => {
-            if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+            if (offset == (deleteNotNumber(width) * (slides.length - 1))) {
                 offset = 0;
             } else {
-                offset += +width.slice(0, width.length - 2); 
+                offset += deleteNotNumber(width); 
             }
     
             slidesField.style.transform = `translateX(-${offset}px)`;
@@ -451,9 +456,9 @@ window.addEventListener('DOMContentLoaded', function () {
     
         prev.addEventListener('click', () => {
             if (offset == 0) {
-                offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+                offset = deleteNotNumber(width) * (slides.length - 1);
             } else {
-                offset -= +width.slice(0, width.length - 2);
+                offset -= deleteNotNumber(width);
             }
     
             slidesField.style.transform = `translateX(-${offset}px)`;
@@ -479,7 +484,7 @@ window.addEventListener('DOMContentLoaded', function () {
                 const slideTo = e.target.getAttribute('data-slide-to');
     
                 slideIndex = slideTo;
-                offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+                offset = deleteNotNumber(width) * (slideTo - 1);
     
                 slidesField.style.transform = `translateX(-${offset}px)`;
     
