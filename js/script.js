5fcbc6a3686b5814a303974ea5c540de626659fb
Alyeev <markmarkul123@gmail.com>
2021-02-13 22:01:30 +0400

    Add calc

--- js/script.js
+++ js/script.js
@@ -132,16 +132,16 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 300000);
+    // const modalTimerId = setTimeout(openModal, 300000);
     // Изменил значение, чтобы не отвлекало
 
-    function showModalByScroll() {
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-            openModal();
-            window.removeEventListener('scroll', showModalByScroll);
-        }
-    }
-    window.addEventListener('scroll', showModalByScroll);
+    // function showModalByScroll() {
+    //     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+    //         openModal();
+    //         window.removeEventListener('scroll', showModalByScroll);
+    //     }
+    // }
+    // window.addEventListener('scroll', showModalByScroll);
 
 
     //Using classes for cards
@@ -242,18 +242,18 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
     // using axios
-    axios.get('http://localhost:3000/menu')
-        .then(data => {
-            data.data.forEach(({
-                img,
-                altimg,
-                title,
-                descr,
-                price
-            }) => {
-                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-            });
-        });
+    // axios.get('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.data.forEach(({
+    //             img,
+    //             altimg,
+    //             title,
+    //             descr,
+    //             price
+    //         }) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
 
 
     // Forms
@@ -499,4 +499,79 @@ window.addEventListener('DOMContentLoaded', function () {
             });
         });
 
+
+        //Calc
+
+        const result = document.querySelector('.calculating__result span');
+        let sex = 'female', 
+            height, weight, age, 
+            ratio = 1.375;
+
+        function calcTotal() {
+            if (!sex || !height || !weight || !age || !ratio){
+                result.textContent = '____';
+                return; //if false please stop function
+            }
+
+            if (sex === 'female'){
+                result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+            }else{
+                result.textContent = Math.round(88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age) * ratio);
+            }
+        }
+
+        calcTotal();
+
+        function getStaticInfo(parentSelector, activeClass){
+            const elements = document.querySelectorAll(`${parentSelector} div`);
+
+            elements.forEach(elem =>{
+                elem.addEventListener('click', (e)=>{
+                    //if we click button with data atribute take pls its target else take id for male/female
+                    if (e.target.getAttribute('data-ratio')) {
+                        ratio = +e.target.getAttribute('data-ratio');
+                    } else {
+                        sex = e.target.getAttribute('id');
+                    }
+    
+    
+                    elements.forEach(elem =>{
+                        elem.classList.remove(activeClass);
+                    });
+    
+                    e.target.classList.add(activeClass);
+    
+                    calcTotal();
+                });
+            });
+
+        }
+
+        getStaticInfo('#gender', 'calculating__choose-item_active');
+        getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active');
+
+
+        function getDynamicInfo(selector) {
+            const input = document.querySelector(selector);
+
+            input.addEventListener('input', () => {
+                switch (input.getAttribute('id')) {
+                    case 'height':
+                        height = +input.value;
+                        break;
+                    case 'weight':
+                        weight = +input.value;
+                        break;
+                    case 'age':
+                        age = +input.value;
+                        break;
+                }
+
+                calcTotal();
+            });
+        }
+
+        getDynamicInfo('#height');
+        getDynamicInfo('#weight');
+        getDynamicInfo('#age');
 });
